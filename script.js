49d224ca4dea6f741ae4a6a46869e73aedeb1e4f
Lei <Lei>
2016-06-09 17:28:54 -0400

    get rid of ajax call

--- script.js
+++ script.js
@@ -4,10 +4,6 @@ $(document).ready(function() {
   optionA.hide();
   optionB.hide();
 
-  var propertyType = $('.propertyType');
-  var landsize = $('.landsize');
-  var metersize = $('.metersize');
-
   var radio = $('input[name=reading]');
   radio.on('click', showOptions);
 
@@ -15,13 +11,58 @@ $(document).ready(function() {
     if(radio.filter(':checked').val() === 'reading'){
       optionA.show();
       optionB.hide();
-      $('.submit').on('click', readingCalc);
+      $('.submit').on('click', {option: 'reading'}, calculate);
     } else{
       optionB.show();
       optionA.hide();
+      $('.submit').on('click', {option: 'consumption'}, calculate);
     }
   }
 
+//speadsheet -- Hidden list table
+  var list = {
+    "RateClasses":
+      ["RESIDENTIAL","LDM","COMMERCIAL","INSTITUTIONAL","INDUSTRIAL"],
+    "UNITS":
+      [1,2,3,4,5,6,0,0,0,0],
+    "YesNo":
+      ["YES","NO"],
+    "TIER":
+      [7,8,10,10,215,6750,43000,50000,0,0],
+    "MeterSize":
+      ["16mm","19mm","25mm","40mm","50mm","76mm","100mm","150mm","200mm","250mm"],
+    "WaterConnect":
+      [14.46,21.69,36.15,72.31,115.7,253.06,433.83,1012.28,1735.21,2169.29],
+    "WasteWaterConnect":
+      [12.25,18.36,30.59,61.18,97.89,214.13,367.07,856.52,1468.32,1835.91],
+    "StormUnder":
+      [14.92,14.92,14.92,14.92,14.92,0,0,0,0,0],
+    "StormOver":
+      [124.19,124.19,124.19,124.19,124.19,0,0,0,0,0]
+  };
+
+//waterRatesAndTiers Table & wastewaterRatesAndTiers Table
+  var rates = {
+    "ProratedTier":[7,8,10,10,215,6750,43000,50000],
+    "Consumption":[7,0,0,0,0,0,0,0],
+    "TierRange":[0,8,16,26,36,251,7001,50001],
+    "WaterRate":[0,2.0596,2.6481,2.9424,1.1182,1.0593,0.9652,0.8593],
+    "WaterWasteRate": [0,1.8305,2.3536,2.6151,0.9936,0.9414,0.8578,0.7636]
+  };
+  var ratesLDM = {
+    "ProratedTier":[7,32,40,40,860,27000,172000,200000],
+    "Consumption":[7,0,0,0,0,0,0,0],
+    "TierRange":[0,8,40,80,120,980,27980,199980],
+    "WaterRate":[0,2.0596,2.6481,2.9424,1.1182,1.0593,0.9652,0.8593],
+    "WaterWasteRate": [0,1.8305,2.3536,2.6151,0.9936,0.9414,0.8578,0.7636]
+  }
+
+  var propertyType = $('.propertyType');
+  var landsize = $('.landsize');
+  var metersize = $('.metersize');
+  var currReading = $('#currReading');
+  var lastReading = $('#lastReading');
+  var consumptionInput = $('#consumption');
 
   var PRORATIONVALUE = 1.0;
   var FIREPROTECTIONCHARGE = 1.48;
@@ -29,35 +70,35 @@ $(document).ready(function() {
   var wasteWaterConnectionCharge;
   var stormDrainageCharge;
   var customerAssisFund;
-  var totalInfraCharge;
-  var currReading = $('#currReading');
-  var lastReading = $('#lastReading');
+  var totalInfraCharge = 0;
+
   var consumption = 0;;
   var LDMwaterUsage = 0;
   var LDMwasteWaterUsage = 0;
   var waterUsage = 0;
   var wasteWaterUsage=0;
   var totalUsageCharge=0;
+  var totalCharge = 0;
 
   function totalInfrastructureCharges(data) {
-      waterConnectionCharge = data.WaterConnect[data.MeterSize.indexOf(metersize.val())];
-      wasteWaterConnectionCharge = data.WasteWaterConnect[data.MeterSize.indexOf(metersize.val())];
+    waterConnectionCharge = data.WaterConnect[data.MeterSize.indexOf(metersize.val())];
+    wasteWaterConnectionCharge = data.WasteWaterConnect[data.MeterSize.indexOf(metersize.val())];
 
-      if(parseFloat(landsize.val()) > 0.4) {
-        stormDrainageCharge = data.StormOver[data.RateClasses.indexOf(propertyType.val())] * parseFloat(landsize.val()) * PRORATIONVALUE;
-      } else {
-        stormDrainageCharge = data.StormUnder[data.RateClasses.indexOf(propertyType.val())] * PRORATIONVALUE;
-      }
+    if(parseFloat(landsize.val()) > 0.4) {
+      stormDrainageCharge = data.StormOver[data.RateClasses.indexOf(propertyType.val())] * parseFloat(landsize.val()) * PRORATIONVALUE;
+    } else {
+      stormDrainageCharge = data.StormUnder[data.RateClasses.indexOf(propertyType.val())] * PRORATIONVALUE;
+    }
 
-      if(propertyType.val() === 'RESIDENTIAL' || propertyType.val() === 'LDM') {
-        customerAssisFund = 0.25;
-      } else {
-        customerAssisFund = 0
-      }
+    if(propertyType.val() === 'RESIDENTIAL' || propertyType.val() === 'LDM') {
+      customerAssisFund = 0.25;
+    } else {
+      customerAssisFund = 0
+    }
 
-      totalInfraCharge =
-        waterConnectionCharge + wasteWaterConnectionCharge + stormDrainageCharge
-        + customerAssisFund + FIREPROTECTIONCHARGE;
+    totalInfraCharge =
+      waterConnectionCharge + wasteWaterConnectionCharge + stormDrainageCharge
+      + customerAssisFund + FIREPROTECTIONCHARGE;
     }
 
 
@@ -67,44 +108,53 @@ $(document).ready(function() {
     });
     var tierRangeIndex = data.TierRange.indexOf(tierRange);
 
-    let i;
-    let cost = 0;
+    var i;
+    var waterCost = 0;
+    var wasteWaterCost = 0;
     for(i = 0; i <= tierRangeIndex; i++){
       if(i == tierRangeIndex) {
-        cost += (consumption - (data.TierRange[i]-1)) * data.Rate[i];
+        waterCost += (consumption - (data.TierRange[i]-1)) * data.WaterRate[i];
+        wasteWaterCost += (consumption - (data.TierRange[i]-1)) * data.WaterWasteRate[i];
       } else{
-        cost += (data.ProratedTier[i] * data.Rate[i]);
+        waterCost += (data.ProratedTier[i] * data.WaterRate[i]);
+        wasteWaterCost += (data.ProratedTier[i] * data.WaterWasteRate[i]);
       }
     }
-    if (data.TierRange[2] === 40 && data.Rate[1] === 2.0596) {//check if the file is WaterRatesAndTiersLDM
-      LDMwaterUsage = cost.toFixed(2);
-    } else if (data.TierRange[2] === 40 && data.Rate[1] === 1.8305){//check if the file is waterwasteRatesAndTiersLDM
-      LDMwasteWaterUsage = cost.toFixed(2);
-    } else if (data.TierRange[2] === 16 && data.Rate[1] === 2.0596) { //check if the file is WaterRatesAndTiers
-      waterUsage = cost.toFixed(2);
-    } else if (data.TierRange[2] === 16 && data.Rate[1] === 1.8305){
-      wasteWaterUsage = cost.toFixed(2);
+
+    if (data.TierRange[2] === 40 && data.WaterRate[1] === 2.0596) {//check if the file is WaterRatesAndTiersLDM
+      LDMwaterUsage = waterCost.toFixed(2);
+    }
+    if (data.TierRange[2] === 40 && data.WaterWasteRate[1] === 1.8305){//check if the file is waterwasteRatesAndTiersLDM
+      LDMwasteWaterUsage = wasteWaterCost.toFixed(2);
+    }
+    if (data.TierRange[2] === 16 && data.WaterRate[1] === 2.0596) { //check if the file is WaterRatesAndTiers
+      waterUsage = waterCost.toFixed(2);
+    }
+    if (data.TierRange[2] === 16 && data.WaterWasteRate[1] === 1.8305){
+      wasteWaterUsage = wasteWaterCost.toFixed(2);
     }
   }
 
-  function readingCalc(){
+  function calculate(event){
     //total infrastructure charges
-    consumption = parseFloat(currReading.val()) - parseFloat(lastReading.val());
-    $.get('data/list.json', totalInfrastructureCharges);
-    console.log(totalInfraCharge);
-    // total usage charges
+    if(event.data.option === 'reading'){
+      consumption = parseFloat(currReading.val()) - parseFloat(lastReading.val());
+    } else {
+      consumption = parseFloat(consumptionInput.val());
+    }
+
+    totalInfrastructureCharges(list);
 
     if(propertyType.val() === 'LDM') {
-      $.get('data/WaterRatesAndTiersLDM.json', usageCharges);
-      $.get('data/waterwasteRatesAndTiersLDM.json', usageCharges);
-      totalUsageCharge = LDMwaterUsage + LDMwasteWaterUsage;
-      // console.log(LDMwaterUsage);
+      usageCharges(ratesLDM);
+      totalUsageCharge = parseFloat(LDMwaterUsage) + parseFloat(LDMwasteWaterUsage);
     } else {
-      $.get('data/WaterRatesAndTiers.json', usageCharges);
-      $.get('data/waterwasteRatesAndTiers.json', usageCharges);
-      totalUsageCharge = waterUsage + wasteWaterUsage;
+      usageCharges(rates);
+      totalUsageCharge = parseFloat(waterUsage) + parseFloat(wasteWaterUsage);
     }
-    $('div.result').html(totalUsageCharge);
+
+    totalCharge = (totalUsageCharge + totalInfraCharge).toFixed(2);
+    $('div.result').html('Your estimated bill will be: $' + totalCharge);
   }
 
 });
