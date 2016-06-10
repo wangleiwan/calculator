d545189837e06198a7a6d72b707344d7ad798bd7
Lei Wang <wangleiwan@hotmail.com>
2016-06-09 23:03:32 -0400

    add comments

--- script.js
+++ script.js
@@ -136,7 +136,6 @@ $(document).ready(function() {
   }
 
   function calculate(event){
-    //total infrastructure charges
     if(event.data.option === 'reading'){
       consumption = parseFloat(currReading.val()) - parseFloat(lastReading.val());
     } else {
